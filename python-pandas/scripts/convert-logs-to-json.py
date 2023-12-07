a031b5123e99f1503896de2a4147be04ee457d6e
Leszek Sliwko <leszek.sliwko1@digital.homeoffice.gov.uk>
2023-12-07 23:20:31 +0000

    updates

--- python-pandas/scripts/convert-logs-to-json.py
+++ python-pandas/scripts/convert-logs-to-json.py
@@ -12,14 +12,14 @@ from datetime import datetime
 # {
 #     "timestamp": "2022-12-23T12:34:56Z",
 #     "level": "error",
-#     "message": "APP_ERR_204:There was an error processing the request",
-#     "request_id": "1234567890",
+#     "message": "APP_ERR_204:There was an error processing the request"
 # }
 
 def log_line_slf4j(log_line):
-    timestamp = log_line.split(' ')[0][:23]
-    level = log_line.split(' ')[1].lower()
-    message = log_line.split(' ', 2)[2].strip()
+    log_line_segments = log_line.split(' ', 2)  # split into no more than three segments
+    timestamp = log_line_segments[0][:23]  # read timestamp, limit to 23 characters
+    level = log_line_segments[1].lower()
+    message = log_line_segments[2].strip()  # copy everything after 2nd segment
     return timestamp, level, message
 
 
@@ -29,10 +29,10 @@ def append_logs(log_dataframe, log_filename, func_log_line):
     log__file = open(log_filename, 'r')
     for log_line in log__file.read().splitlines():
         if not log_line: continue  # ignore empty lines
-
         # print(f'{log_line}')
+
         try:
-            (timestamp, level, message) = func_log_line(log_line)
+            (timestamp, level, message) = func_log_line(log_line)  # parse line and return three values
         except Exception as error:
             error_message = f'Error parsing line {line_no} from {log_filename}: {log_line}'
             raise Exception(error_message) from error
@@ -43,15 +43,16 @@ def append_logs(log_dataframe, log_filename, func_log_line):
             raise Exception(error_message)
 
         # sanity check
-        timestamp_date = ''
         try:
             timestamp_date = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S,%f")
         except Exception as error:
             error_message = f'Illegal timestamp {timestamp} parsed from log line: {log_line}'
             raise Exception(error_message)
 
+        # append to dataframe
         log_dataframe.loc[-1] = [timestamp_date, level, message]
         log_dataframe.index = log_dataframe.index + 1
+
         line_no = line_no + 1
 
     log__file.close()
@@ -64,7 +65,7 @@ df = pd.DataFrame({'timestamp': [], 'level': [], 'message': []})
 for log_file in glob.glob('logs/*.log'):
     append_logs(df, log_file, log_line_slf4j)
 
-df\
+df \
     .sort_values(by=['timestamp']) \
     .to_excel('logs-20151018.xlsx',
               index=False)
