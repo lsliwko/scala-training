2602075fbc6b549e096a726070b8cdf3f4f8aa8e
Leszek Sliwko <lsliwko@gmail.com>
2023-12-16 09:54:16 +0000

    updates

--- python-pandas/scripts/1_basics/1_4_GlobalNonlocalScope.py
+++ python-pandas/scripts/1_basics/1_4_GlobalNonlocalScope.py
@@ -14,7 +14,7 @@ def outer():
 
 outer()
 print("global:", variable_x)
-
+print('---')
 
 # inner: 2
 # outer: 1
@@ -25,7 +25,7 @@ def outer():
     variable_x = 1  # outer scope
 
     def inner():
-        nonlocal variable_x  # outer scope (BY NONLOCAL!!!)
+        nonlocal variable_x  # outer scope (!!!)
         variable_x = 2
         print("inner:", variable_x)
 
@@ -35,7 +35,31 @@ def outer():
 
 outer()
 print("global:", variable_x)
+print('---')
 
 # inner: 2
 # outer: 2
 # global: 0
+
+x = 0
+
+
+def outer():
+    variable_x = 1  # outer scope
+
+    def inner():
+        global variable_x  # global scope(!!!)
+        variable_x = 2
+        print("inner:", variable_x)
+
+    inner()
+    print("outer:", variable_x)
+
+
+outer()
+print("global:", variable_x)
+print('---')
+
+# inner: 2
+# outer: 1
+# global: 2
