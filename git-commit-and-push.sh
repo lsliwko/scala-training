b0d1f93216f6e698c6ed70f7fb8877f7d19a9a70
Leszek Sliwko <lsliwko@gmail.com>
2024-01-14 16:52:52 +0000

    updates

---
+++ git-commit-and-push.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#git config filter.compress.clean gzip
+#git config filter.compress.smudge gzip -d
+
+GIT_SSH_COMMAND="ssh -i ~/.ssh/lsliwko_rsa" git status
+GIT_SSH_COMMAND="ssh -i ~/.ssh/lsliwko_rsa" git add *
+GIT_SSH_COMMAND="ssh -i ~/.ssh/lsliwko_rsa" git commit -m updates
+GIT_SSH_COMMAND="ssh -i ~/.ssh/lsliwko_rsa" git push
