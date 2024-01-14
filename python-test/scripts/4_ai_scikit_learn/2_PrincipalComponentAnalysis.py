18426fdd55d9bc7185b2200cca28f4e45083eec7
Leszek Sliwko <lsliwko@gmail.com>
2024-01-14 16:51:20 +0000

    updates

---
+++ python-test/scripts/4_ai_scikit_learn/2_PrincipalComponentAnalysis.py
@@ -0,0 +1,14 @@
+# 1) Singular Value Decomposition
+# https://www.youtube.com/playlist?list=PLWhu9osGd2dB9uMG5gKBARmk73oHUUQZS
+
+
+# 2) Principal Component Analysis, or PCA, is a dimensionality reduction method that is often used to
+# reduce the dimensionality of large data sets, by transforming a large set of variables into a smaller
+# one that still contains most of the information in the large set.
+
+# https://youtu.be/FgakZw6K1QQ
+# https://youtu.be/HMOI_lkzW08
+
+
+# https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.PCA.html
+
