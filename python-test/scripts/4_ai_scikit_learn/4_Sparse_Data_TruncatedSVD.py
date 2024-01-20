17d42c0b2b16b4001dd9096c00173f51f3297a2b
Leszek Sliwko <lsliwko@gmail.com>
2024-01-20 22:00:03 +0000

    updates

---
+++ python-test/scripts/4_ai_scikit_learn/4_Sparse_Data_TruncatedSVD.py
@@ -0,0 +1,6 @@
+# What are Sparse Datasets? The features of a dataset can be sparse or dense. If the data stored for
+# a particular feature contains mostly zeroes, it is referred to as a sparse feature. If the feature
+# is populated mostly with non-zero values, it is dense.
+# https://scikit-learn.org/stable/modules/generated/sklearn.decomposition.TruncatedSVD.html
+
+# TODO
