ea78bca427f21093bb53950e39a4a9de1b3329a8
Leszek Sliwko <lsliwko@gmail.com>
2024-01-28 21:07:58 +0000

    updates

--- python-test/fix_metadata_python.sh
+++ python-test/fix_metadata_python.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 find /opt/homebrew/lib/python3.11/site-packages -empty -type d -delete
+
+pip3 uninstall jsonschema
+pip3 install jsonschema
+
+pip3 uninstall jupyterlab
+pip3 install jupyterlab
+jupyter lab build
+jupyter lab
--- python-test/run_jupyter_notebook.sh
+++ python-test/run_jupyter_notebook.sh
@@ -2,4 +2,7 @@
 
 pip3 install jupyter
 
+export JUPYTER_PATH=/opt/homebrew/share/jupyter
+export JUPYTER_CONFIG_PATH=/opt/homebrew/etc/jupyter jupyter notebook
+
 jupyter notebook
