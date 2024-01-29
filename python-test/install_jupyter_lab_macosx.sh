d4689dc2a3f86a841a687198a7957adf0f6e4929
Leszek Sliwko <leszek.sliwko1@digital.homeoffice.gov.uk>
2024-01-29 16:36:09 +0000

    updates

--- python-test/install_jupyter_lab_macosx.sh
+++ python-test/install_jupyter_lab_macosx.sh
@@ -2,10 +2,15 @@
 
 find /opt/homebrew/lib/python3.11/site-packages -empty -type d -delete
 
+brew install npm
+brew install node
+
 pip3 uninstall jsonschema
 pip3 install jsonschema
 
 pip3 uninstall jupyterlab
 pip3 install jupyterlab
+
 jupyter lab build
+
 jupyter lab
