66e420dba315697cc3c7efe47703a40dfc461db2
Leszek Sliwko <leszek.sliwko1@digital.homeoffice.gov.uk>
2024-02-29 21:46:41 +0000

    updates

--- python-test/scripts/6_sparce_task_data_ai/0_test.py
+++ python-test/scripts/6_sparce_task_data_ai/0_test.py
@@ -3,11 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pyarrow.csv
 
-dataset = pd.read_csv('datapoint-task-test-mini.csv', low_memory=False)
-# dataset = pd.read_csv('datapoint-task-full.csv', low_memory=False)
 
-# print(dataset.columns[0:4].all(['AVAILABLE NODES COUNT', 'TASK ID', 'CPU REQUIRED', 'MEMORY REQUIRED']))
-# sanity check
-if list(dataset.columns[0:4]) != ['AVAILABLE NODES COUNT', 'TASK ID', 'CPU REQUIRED', 'MEMORY REQUIRED']:
-    raise Exception('illegal columns')
+print(np.array([12.81212, 1000.1213322, 10.23123]).round(4))
+
 
