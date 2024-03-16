1e04726c54ebaaaadb9ae3d6b719b10403fadb9c
Leszek Sliwko <lsliwko@gmail.com>
2024-03-16 09:12:39 +0000

    updates

--- git-commit-and-push.sh
+++ git-commit-and-push.sh
@@ -5,5 +5,5 @@
 
 GIT_SSH_COMMAND="ssh -i ~/.ssh/lsliwko_rsa" git status
 GIT_SSH_COMMAND="ssh -i ~/.ssh/lsliwko_rsa" git add *
-GIT_SSH_COMMAND="ssh -i ~/.ssh/lsliwko_rsa" git commit -m updates
+GIT_SSH_COMMAND="ssh -i ~/.ssh/lsliwko_rsa" git commit -u -m updates
 GIT_SSH_COMMAND="ssh -i ~/.ssh/lsliwko_rsa" git push
