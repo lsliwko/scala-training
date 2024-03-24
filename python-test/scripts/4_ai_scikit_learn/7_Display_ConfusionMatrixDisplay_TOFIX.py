f5457cefee35edd70e732ede3ec5e56b28b5e0a2
Leszek Sliwko <lsliwko@gmail.com>
2024-03-24 11:37:15 +0100

    updates

--- python-test/scripts/4_ai_scikit_learn/7_Display_ConfusionMatrixDisplay_TOFIX.py
+++ python-test/scripts/4_ai_scikit_learn/7_Display_ConfusionMatrixDisplay_TOFIX.py
@@ -4,11 +4,12 @@ from sklearn.datasets import fetch_openml
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import ConfusionMatrixDisplay
 
+
 digits = fetch_openml('mnist_784', version=1)
 X, y = digits.data, digits.target
 rf_clf = RandomForestClassifier(max_depth=5, random_state=42)
 rf_clf.fit(X, y)
 
-ConfusionMatrixDisplay.from_estimator(rf_clf, X, y)
-
+ax = plt.gca()
+ConfusionMatrixDisplay.from_estimator(rf_clf, X, y, ax=ax)
 plt.show()
