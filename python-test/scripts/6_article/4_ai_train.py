49a890ccc42e69efe7ad8dd1b010f626e4353805
Leszek Sliwko <lsliwko@gmail.com>
2024-04-25 22:49:27 +0100

    updates

--- python-test/scripts/6_article/4_ai_train.py
+++ python-test/scripts/6_article/4_ai_train.py
@@ -3,8 +3,22 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pyarrow.csv
 
-dataset = pd.read_csv('/Users/lsliwko/workspace/MASB-DATA/with-header/datapoint-task-merged-cat-encoded-1-200000.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[4:].values
+print("Loading dataset...")
+dataset = pd.read_csv('/Users/lsliwko/workspace/MASB-DATA/with-header/datapoint-task-merged-cat-encoded-1-5000.csv')
+X = dataset.iloc[:, 4:].values
+y = dataset.iloc[:, 0].values
+print(f"Dataset {X.shape}")
 
-print(X)
+from sklearn.model_selection import train_test_split
+
+X_test, X_train, y_test, y_train = train_test_split(X, y, test_size=0.2, random_state=0)
+print(f"TrainSet {X_train.shape}")
+
+print("Training...")
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+print("Trained")
+
+y_pred = regressor.predict(X_test)
+
