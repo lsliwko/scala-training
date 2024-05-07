044591f44e89be35fde09f34b8c44a06ef97e90d
Leszek Sliwko <lsliwko@gmail.com>
2024-05-07 22:52:00 +0100

    updates

--- python-test/scripts/6_article/4_ai_train.py
+++ python-test/scripts/6_article/4_ai_train.py
@@ -10,7 +10,7 @@ from time import perf_counter
 from sklearn.model_selection import train_test_split
 
 print("Loading dataset...")
-dataset = pd.read_csv('/Users/lsliwko/workspace/MASB-DATA/datapoint-task-merged-no-dups-cat-encoded.csv')
+dataset = pd.read_csv('/Users/lsliwko/workspace/MASB-DATA/datapoint-task-merged-no-dups-cat-encoded-sorted.csv')
 X = dataset.iloc[:, 4:].values
 y = dataset.iloc[:, 0].values
 print(f"Dataset size = {X.shape}")
@@ -24,10 +24,12 @@ X_test = X
 y_test = y
 print(f"TrainSet size = {X_train.shape}")
 
+
+
+
 regressor = LinearRegression()
 # regressor = DecisionTreeRegressor(random_state=0)
 # regressor = LogisticRegression()
-
 # regressor = SVR(kernel = 'rbf')
 # regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
 # regressor = KNeighborsRegressor(n_neighbors=5)
@@ -53,3 +55,6 @@ dataset.insert(1, "AVAILABLE NOTES PREDICTED", y_pred)
 dataset.to_csv('/Users/lsliwko/workspace/MASB-DATA/datapoint-task-merged-no-dups-cat-encoded-predicted.csv',
                index=False)
 print(f"Saved results in {(perf_counter() - start) * 1000:.0f} ms")
+
+
+
