7aafd1fa1fc4a688a8a33e1b79b876d17ea835dd
Leszek Sliwko <lsliwko@gmail.com>
2026-07-23 01:57:55 +0100

    updates

---
+++ scala-misc/src/main/scala/test/MainApp.scala
@@ -0,0 +1,35 @@
+import java.util
+
+object MainApp extends App {
+
+  println(
+    minTimeToVisitAllPoints(points = Array(Array(1,1),Array(3,4),Array(-1,0)) )
+  )
+
+  def minTimeToVisitAllPoints(points: Array[Array[Int]]): Int = {
+    Range(0, points.length-1).map( index => {
+      //println("A = " + util.Arrays.toString(points(index)))
+      //println("B = " + util.Arrays.toString(points(index+1)))
+
+      val distance = distanceBetweenPoints(
+        points(index),
+        points(index + 1)
+      )
+
+      //println(distance)
+      distance
+    }
+    ).sum
+
+
+  }
+
+  def distanceBetweenPoints(pointA : Array[Int], pointB : Array[Int]) : Int = {
+    Math.max(
+      Math.abs(pointA(0) - pointB(0)),
+      Math.abs(pointA(1) - pointB(1))
+    )
+  }
+
+
+}
